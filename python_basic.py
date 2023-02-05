7a5fc96f21118e80f2bdf1450cd43f8449d28332
yungshun317 <seraph317@gmail.com>
2023-02-05 15:20:36 +0800

    [FEAT] Perform word count using loop, `count`, `collections.Counter`, lambda function, and regular expression

--- python_basic.py
+++ python_basic.py
@@ -15,3 +15,9 @@ def terminal_size():
     return tw, th
 
 print("Number of columns and rows:", terminal_size())
+
+import sys
+
+print("This is the name/path of the script:", sys.argv[0])
+print("Number of arguments:", len(sys.argv))
+print("Argument list:", str(sys.argv))
