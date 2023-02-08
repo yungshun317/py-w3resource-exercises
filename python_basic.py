293cdc5bd8bd65aaed8d66b5cd755ebba4b727b4
yungshun317 <seraph317@gmail.com>
2023-02-08 08:49:57 +0800

    [FEAT] Get absolute and relative file paths

--- python_basic.py
+++ python_basic.py
@@ -31,4 +31,20 @@ print("Access time:", time.ctime(os.path.getatime(__file__)))
 print("Modified time:", time.ctime(os.path.getmtime(__file__)))
 print("Change time:", time.ctime(os.path.getctime(__file__)))
 print("Size:", os.path.getsize(__file__))
-        
+
+# Problem 108
+# [1] `os.path` module & `os.getcwd()`
+import os.path
+
+print("Absolute file path:", os.path.abspath(__file__))
+
+current_working_directory = os.getcwd()
+print("Relative file path:", os.path.relpath("/home/yungshun/workspace/py/python-dsa", current_working_directory))
+
+# [2] `os.path` module & `pathlib.Path.cwd()`
+from pathlib import Path
+
+print("Absolute file path:", os.path.abspath(__file__))
+
+current_working_directory = Path.cwd()
+print("Relative file path:", os.path.relpath("/home/yungshun/workspace/py/python-dsa", current_working_directory))
